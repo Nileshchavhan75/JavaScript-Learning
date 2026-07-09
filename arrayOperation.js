38739ca9245b498098b7cba93169708ff3050849
Nilesh Chavhan <nchavhan544@gmail.com>
2026-07-09 14:36:13 +0530

    Added array program

---
+++ arrayOperation.js
@@ -0,0 +1,9 @@
+let mounts = ["january", "july","march","augaust"];
+console.log(mounts);
+mounts.shift();
+mounts.shift();
+console.log(mounts);
+
+mounts.unshift("june");
+mounts.unshift("July");
+console.log(mounts);
