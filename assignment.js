cf5f4b192bc22d10ed63f770e17786afddba8aa4
Nilesh Chavhan <nchavhan544@gmail.com>
2026-07-14 17:27:40 +0530

    Added assignment no 1

---
+++ assignment.js
@@ -0,0 +1,38 @@
+// let num = 50;
+
+// if(num % 10 == 0){
+//     console.log("Good");
+// }else{
+//     console.log("Bad");
+// }
+
+// let name = prompt("Enter your name: ");
+// let age = prompt("Enter your age: ");
+// let msg = name+" is "+ age +" Year old"
+// alert(msg);
+
+// let str = "Applee";
+
+// if((str[0] == 'a' || str[0] == 'A' )&& str.length > 5){
+//     console.log("Golden");
+// }else{
+//     console.log("Not Golden");
+// }
+
+let a = 5;
+let b = 12;
+let c = 14;
+
+if(a > b){
+    if(a > c){
+        console.log(a, " is Largest");
+    }else{
+        console.log(c, "is largest");
+    }
+}else{
+    if(b > c){
+        console.log(b, " is Largest");
+    }else{
+        console.log(c, "is Largest");
+    }
+}
