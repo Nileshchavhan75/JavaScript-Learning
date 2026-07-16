4004b7d1fe3e681df87e6bb0d1d34cd79e2191a6
Nilesh Chavhan <nchavhan544@gmail.com>
2026-07-16 11:54:10 +0530

    Conditional added

---
+++ conditional.js
@@ -0,0 +1,24 @@
+// let marks = 75;
+
+// if (marks >= 90) {
+//     console.log("A+");
+// } else if (marks >= 70) {
+//     console.log("A");
+// } else if (marks >= 60) {
+//     console.log("B+");
+// } else {
+//     console.log("Fail");
+// }
+
+
+let price = 50;
+
+if(price == 250){
+    console.log("popcorn size XL");
+}else if(price == 200){
+    console.log("popcorn size L")
+}else if(price == 100){
+    console.log("M");
+}else if(price == 50){
+    console.log("S");
+}
