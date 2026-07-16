d08321683c284c5c05c024d7594d5c90f2dadd00
Nilesh Chavhan <nchavhan544@gmail.com>
2026-07-16 11:53:34 +0530

    Assignment 3 added

---
+++ assignment3.js
@@ -0,0 +1,61 @@
+// let arr = [14,12,5,8,44,16,77,55];
+
+// let num = 6;
+
+// function getElement(arr, num){
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i]> num){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
+// getElement(arr, num);
+
+// let str = "abcdabcdefgggh";
+
+// function getUniqueElement(str){
+//     let ans = "";
+//     for(let i = 0; i<str.length; i++){
+//         let getChar = str[i];
+//         if(ans.indexOf(getChar) == -1){
+//             ans += getChar;
+//         }
+//     }
+//     return ans;
+// }
+// let newStr = getUniqueElement(str);
+// console.log(newStr);
+
+// let name = "rutuja";
+
+
+// function countVowels(name){
+//      let count = 0;
+//     for(let i = 0; i<name.length; i++){
+//         if(name.charAt(i) == "a"||name.charAt(i)== "e"||name.charAt(i)=="i"||name.charAt(i)=="o"||name.charAt(i)== "u"){
+//             count++;
+//         }
+//     }
+//      return count;
+// }
+
+// let ans = countVowels(name);
+
+// console.log("Number of Vowels: ",ans);
+
+
+
+// function printingGreet(facn, count){
+//     for(let i = 1; i<count; i++){
+//         facn();
+//     }
+// }
+
+// let facn = function gretting(){
+//     console.log("Rutuja and Nilesh");
+// }
+
+// printingGreet(facn,5);
+
+
